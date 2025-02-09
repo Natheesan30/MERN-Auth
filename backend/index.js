7f30d765e6196d3b8a902c073ac2a9f9decdb1f2
Natheesan30 <2020sp092@univ.jfn.ac.lk>
2025-02-09 19:56:27 +0530

    Signup endpoint and check with postman.

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 import express from 'express'
 import { connectDB } from './db/connectDB.js';
 import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res)=>{
-    res.send('Hello World!')
-})
+app.use(express.json());
 
-app.listen(3000, ()=>{
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, ()=>{
     connectDB();
-    console.log('Server is running on 3000');   
+    console.log(`Server is running on ${PORT}`);   
 })
-
-// mongodb+srv://nsaran755:rH48fzFPKPH8VZLt@cluster0.joyup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
