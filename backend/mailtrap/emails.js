33381db81a99cbe45e7f3b341ed78f24bbd23ebb
Natheesan30 <2020sp092@univ.jfn.ac.lk>
2025-02-10 11:28:07 +0530

    Sending verify account email.

---
+++ backend/mailtrap/emails.js
@@ -0,0 +1,20 @@
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import { mailTrapClient, sender } from "./mailtrap.config.js"
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+    const recipient = [{email}]
+    try {
+        const response= await mailTrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "verify your email",
+            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}",verificationToken),
+            category: "Email verification"
+        })
+        console.log("Email sent successfully", response);
+        
+    } catch (error) {
+        console.error(`Error sending verification`, error);
+        throw new Error(`Error sending verification email: ${error}`)
+    }
+}
