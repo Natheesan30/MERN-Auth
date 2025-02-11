62f0aae9e60d1d81e3f7e1ba7095c486a7479554
Natheesan30 <2020sp092@univ.jfn.ac.lk>
2025-02-11 21:40:41 +0530

    Check the auth endpoint and finish the backend part.

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,15 @@ import express from 'express'
 import { connectDB } from './db/connectDB.js';
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json());// allow us to parse incoming requests:req.body
+app.use(cookieParser());// allow us to parse incoming cookies
 
 app.use('/api/auth', authRoutes);
 
