cde87aa8c89a1457b3944c370d6d4bef51e6a2fc
Natheesan30 <2020sp092@univ.jfn.ac.lk>
2025-02-15 11:37:24 +0530

    almost finish the frontend part.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,48 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import FloatingShape from "./components/FloatingShape"
 import SignUpPage from "./pages/SignUpPage"
 import LoginPage from "./pages/LoginPage"
+import EmailVerificationPage from "./pages/EmailVerificationPage"
+import {Toaster} from 'react-hot-toast'
+import { useAuthStore } from "./store/authStore"
+import { useEffect } from "react"
+import DashboardPage from "./pages/DashboardPage"
+import LoadingSpinner from "./components/LoadingSpinner"
+import ForgotPasswordPage from "./pages/ForgotPasswordPage"
+import ResetPasswordPage from "./pages/ResetPasswordPage"
 
+// protect routes that require authentication
+const ProtectedRoute = ({ children }) => {
+	const { isAuthenticated, user } = useAuthStore();
+
+	if (!isAuthenticated) {
+		return <Navigate to='/login' replace />;
+	}
+
+	if (!user.isVerified) {
+		return <Navigate to='/verify-email' replace />;
+	}
+
+	return children;
+};
+// redirect routes that require authentication
+const RedirectAuthenticatedUser = ({ children }) => {
+	const { isAuthenticated, user } = useAuthStore();
+
+	if (isAuthenticated && user.isVerified) {
+		return <Navigate to='/' replace />;
+	}
+
+	return children;
+};
 
 function App() {
+  const {isCheckingAuth, checkAuth} = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  },[checkAuth])
+
+  if(isCheckingAuth) return <LoadingSpinner/>;
   
   return (
     <div className='min-h-screen bg-gradient-to-br
@@ -15,10 +53,26 @@ function App() {
 			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
 
       <Routes>
-          <Route path="/" element={"Home"}></Route>
-          <Route path="/signup" element={<SignUpPage/>}></Route>
-          <Route path="/login" element={<LoginPage/>}></Route>
+          <Route path="/" element={<ProtectedRoute>
+            <DashboardPage/>
+          </ProtectedRoute>}></Route>
+          <Route path="/signup" element={<RedirectAuthenticatedUser>
+            <SignUpPage/>
+          </RedirectAuthenticatedUser>}></Route>
+          <Route path="/login" element={<RedirectAuthenticatedUser>
+            <LoginPage/>
+          </RedirectAuthenticatedUser>}></Route>
+          <Route path="/verify-email" element={<EmailVerificationPage/>}></Route>
+          <Route path="/forgot-password" element={<RedirectAuthenticatedUser>
+            <ForgotPasswordPage/>
+          </RedirectAuthenticatedUser>}></Route>
+
+          <Route path="/reset-password/:token" 
+          element={<RedirectAuthenticatedUser>
+            <ResetPasswordPage/>
+          </RedirectAuthenticatedUser>}/>
       </Routes>
+      <Toaster/>
 </div>
  
   )
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,18 +1,28 @@
 import { motion } from "framer-motion";
 import Input from "../components/Input";
-import {Mail, User, Lock} from 'lucide-react'
+import {Mail, User, Lock, Loader} from 'lucide-react'
 import { useState } from "react";
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+import { useAuthStore } from "../store/authStore";
 
 const SignUpPage = () => {
 
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const {signup, error, isLoading} = useAuthStore();
+  const navigate = useNavigate();
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
+    try {
+      await signup(email, password, name);
+      navigate("/verify-email")
+    } catch (error) {
+      console.log(error);
+      
+    }
   };
   return (
     <motion.div
@@ -48,6 +58,7 @@ const SignUpPage = () => {
           value={password}
           onChange={(e)=> setPassword(e.target.value)}
           />
+          {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
           <PasswordStrengthMeter password={password} />
 
 <motion.button
@@ -58,9 +69,9 @@ const SignUpPage = () => {
 						whileHover={{ scale: 1.02 }}
 						whileTap={{ scale: 0.98 }}
 						type='submit'
-						
+						disabled={isLoading}
 					>
-						Sign up
+						{isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"}
 					</motion.button>
 
         </form>
